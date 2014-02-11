b91682878e2dbef3720152d85cf49fb4ab42ed55
Eddie Lau <tatonlto@gmail.com>
2014-02-11 21:00:40 +0800

    add style to button form

--- app/controllers/alliance_controller.rb
+++ app/controllers/alliance_controller.rb
@@ -1,4 +1,11 @@
 class AllianceController < ApplicationController
   def show
+    @character = Character.offset(rand(Character.count)).first
+  end
+  
+  def update
+    @character = Character.find(params[:id])
+    params[:hero]
+    params[:evil]
   end
 end
--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,4 +1,5 @@
 class QuizController < ApplicationController
   def show
+    @character = Character.offset(rand(Character.count)).first
   end
 end
