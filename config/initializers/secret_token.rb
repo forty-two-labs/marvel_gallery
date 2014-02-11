f46bab4912b2e6745ca7a3e05829f811c3572f78
Eddie Lau <tatonlto@gmail.com>
2014-02-11 19:26:06 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarvelGallery::Application.config.secret_key_base = '7c6fffcda97335866d8a3aac3fa56574c40f14e7c54b4e8e3e382b0a46c5e0c99ec555307aa6d77d4eeb849fbf4c5dd12dd946e48e080ac2e9851e17d506cff3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MarvelGallery::Application.config.session_store :cookie_store, key: '_marvel_gallery_session'
