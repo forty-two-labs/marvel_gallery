3f33fac4019a6051109924bdbc65fde926533538
Eddie Lau <tatonlto@gmail.com>
2014-02-11 19:41:07 +0800

    general template

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 MarvelGallery::Application.routes.draw do
+  get "alliance/show"
+  get "characters/index"
+  get "quiz/show"
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
