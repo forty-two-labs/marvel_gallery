143b3735cb9ce8b0f55df6a498cc492901fc7412
Eddie Lau <tatonlto@gmail.com>
2014-02-12 12:15:19 +0800

    show more characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,6 @@
 class CharactersController < ApplicationController
   def index
-    @character = Character.where("thumbnail_image_url != 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'").order("RANDOM()").limit(8)
+    @character = Character.where("thumbnail_image_url != 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'").order("RANDOM()").limit(15)
   end
   
 end
